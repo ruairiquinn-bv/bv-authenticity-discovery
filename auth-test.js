5a34903760253802177d878848ea0bfe380383c8
Ruairi Quinn <70686487+ruairiquinn-bv@users.noreply.github.com>
2022-04-11 09:51:02 +0100

    Update auth-test.js

--- auth-test.js
+++ auth-test.js
@@ -1,1 +1,9 @@
 console.log('Auth Test Loaded Successfully!');
+
+const reviewsSectionOfThePage = document.getElementById('shopify-product-reviews'); // <-- This grabs the whole review section
+const submitButton = reviewsSectionOfThePage.querySelector('button[type="submit"]');
+
+submitButton.onClick = () => {
+  const reviewAuthor = document.getElementsByName('review[author]')[0].value;
+  console.log('Review submitted!: ', reviewAuthor);
+};
